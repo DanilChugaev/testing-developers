b5cd1f1fcf67292aca2fee05dd40337ae388865d
Danil Chugaev <danilchugaev@mail.ru>
2019-02-19 23:39:46 +0700

    DEPLOY Adds script for run app after deploy

---
+++ .travis/after_deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh web@85.143.223.101 'cd /home/web/www/testing-developers/ && npm ci && npm start && cd /client && npm ci && npm start'
---
+++ .travis/unit-tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo 'unit tests' && return 1
