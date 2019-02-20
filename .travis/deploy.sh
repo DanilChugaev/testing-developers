b96a9264e17ba4da965523f4eee456ed78f3957e
Danil Chugaev <danilchugaev@mail.ru>
2019-02-20 23:08:18 +0700

    DEPLOY Updates scripts 7

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,6 +2,5 @@
 set -e
 
 git config --global push.default simple
-git reset --hard ssh://web@85.143.223.101//home/web/www/testing-developers/
 git remote add production ssh://web@85.143.223.101//home/web/www/testing-developers/
 git push production master
