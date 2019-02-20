30fbe6195c99f70fca136f3329f8f34eb38c341c
Danil Chugaev <danilchugaev@mail.ru>
2019-02-20 22:53:23 +0700

    DEPLOY Updates scripts 4

--- .travis/after_deploy.sh
+++ .travis/after_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh web@85.143.223.101 'cd /home/web/www/testing-developers/ && npm update && npm start && cd /client && npm update && npm start'
+ssh web@85.143.223.101 'cd /home/web/www/testing-developers/ && npm update && npm start && cd client && npm update && npm start'
